aee6bb17f948b7eb3f2f3f6085b3b38626fcf7b0
C.y <yangruihan@vip.qq.com>
2015-05-07 22:43:46 +0800

    15_5_7

--- python/ShowMeTheCode/0000/add_num_on_image.py
+++ python/ShowMeTheCode/0000/add_num_on_image.py
@@ -1,31 +1,36 @@
+#!/usr/bin/python
 #-*- coding:utf-8 -*-
 
 from PIL import Image, ImageDraw, ImageFont
 import os
 
 def add_num(inputImageName, outputImageName, drawNum):
+    img = Image.open(inputImageName)
+
     draw = ImageDraw.Draw(img) #获取图像信息
-    myfont = ImageFont.truetype('C:/windows/fonts/Arial.ttf', size = 40) #获取字体信息
-    fillcolor = "#ff0000" #获取颜色信息 ff0000 代表红色
     width, height = img.size #获取图像长宽
-    draw.text((width - 45, 5), drawNum, font = myfont, fill = fillcolor) #写字
+    myfont = ImageFont.truetype('MONACO.TTF', size = width / 5) #获取字体信息
+    fillcolor = "#ff0000" #获取颜色信息 ff0000 代表红色
+    draw.text((width - width / 5 - 10, 5), drawNum, font = myfont, fill = fillcolor) #写字
     img.save(outputImageName, 'jpeg') #保存图片
 
     return 0
 
 if __name__ == '__main__':
     inputImageName = raw_input('Input image file name: ')
-    while !os.path.exists(inputImageName):
+    while (not os.path.exists(inputImageName)):
         inputImageName = raw_input('Input RIGHT image file name: ')
     
-    outputImageName = raw_input('Output image file name(Default:'result.jpg'): ')
-    if outputImageName == 13:
-        outputImageName = 'result.jpg'
+    outputImageName = raw_input("Output image file name: ")
 
-    if !('.jpg' in outputImageName):
+    if not ('.jpg' in outputImageName):
         outputImageName = outputImageName + '.jpg'
 
     drawNum = raw_input('Write a number to draw: ')
+
+    print(inputImageName)
+    print(outputImageName)
+    print(drawNum)
     
     image = Image.open(inputImageName)
     add_num(inputImageName, outputImageName, drawNum)    
---
+++ python/ShowMeTheCode/0000/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python 
+
+s = 'abc'
+s = raw_input('Enter something')
+print(s)
