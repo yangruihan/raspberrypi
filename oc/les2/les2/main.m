ede35bc06fbe0a7022c86ab4c6a7408cccac906d
Cy <yangruihan@vip.qq.com>
2015-05-24 13:15:56 +0800

    Initial Commit

---
+++ oc/les2/les2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  les2
+//
+//  Created by 杨睿涵 on 15/5/24.
+//  Copyright (c) 2015年 杨睿涵. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
