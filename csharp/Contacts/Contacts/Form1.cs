154fef802ea8b4f1521551b3a26b4ce3bc6bb4d1
C.y <yangruihan@vip.qq.com>
2015-05-27 20:05:55 +0800

    15_5_27

---
+++ csharp/Contacts/Contacts/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Contacts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Contact List v1.0.\nWritten by Y.R.H", "About");
+
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contactDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“contactDBDataSet.People”中。您可以根据需要移动或删除它。
+            this.peopleTableAdapter.Fill(this.contactDBDataSet.People);
+
+        }
+    }
+}
