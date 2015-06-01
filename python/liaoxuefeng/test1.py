5acd8f10abb58414e2753f0a1a063d136b5a576a
Cy <yangruihan@vip.qq.com>
2015-06-01 20:36:27 +0800

    15_6_1

---
+++ python/liaoxuefeng/test1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from enum import Enum
+
+Month = Enum('Month', ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'))
+
+for name, member in Month.__members__.items():
+    print(name, '=>', member, ',', member.value)
