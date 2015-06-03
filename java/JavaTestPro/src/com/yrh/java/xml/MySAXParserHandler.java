c71af299aff1dc0f9ab4c994eff23b7aef7255e7
C.y. <yangruihan@vip.qq.com>
2015-06-04 01:46:34 +0800

    15_6_4

--- java/JavaTestPro/src/com/yrh/java/xml/MySAXParserHandler.java
+++ java/JavaTestPro/src/com/yrh/java/xml/MySAXParserHandler.java
@@ -9,6 +9,8 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 public class MySAXParserHandler extends DefaultHandler {
 
+	int bookIndex = 0;
+
 	/**
 	 * 开始扫描标签
 	 */
@@ -16,8 +18,20 @@ public class MySAXParserHandler extends DefaultHandler {
 	public void startElement(String uri, String localName, String qName,
 			Attributes attributes) throws SAXException {
 		super.startElement(uri, localName, qName, attributes);
+		if (qName.equals("book")) {
+			System.out.println("------------第" + ++bookIndex + "本书------------");
+//			// 第一种方法，知道属性名
+//			System.out.println(attributes.getValue("id"));
+			// 第二种方法，不知道属性名，进行遍历
+			for (int i = 0; i < attributes.getLength(); i++) {
+				System.out.println("属性名：" + attributes.getQName(i) + "属性值：" + attributes.getValue(i));
+			}
+		}
+		if (!qName.equals("bookstore")) {
+		System.out.print("子属性名：" + qName); 
+		}
 	}
-	
+
 	/**
 	 * 结束扫描标签
 	 */
@@ -25,8 +39,24 @@ public class MySAXParserHandler extends DefaultHandler {
 	public void endElement(String uri, String localName, String qName)
 			throws SAXException {
 		super.endElement(uri, localName, qName);
+		if (qName.equals("book")) {
+			System.out.println("------------------------------\n");
+		}
 	}
 	
+	/**
+	 * 扫描节点中的内容
+	 */
+	@Override
+	public void characters(char[] ch, int start, int length)
+			throws SAXException {
+		super.characters(ch, start, length);
+		String str = new String(ch, start, length);
+		if (!str.trim().equals("")) {
+			System.out.println("----属性值：" + str);
+		}
+	}
+
 	/**
 	 * 开始扫描文件
 	 */
@@ -35,7 +65,7 @@ public class MySAXParserHandler extends DefaultHandler {
 		super.startDocument();
 		System.out.println("扫描开始");
 	}
-	
+
 	/**
 	 * 结束扫描文件
 	 */
