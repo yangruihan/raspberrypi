d5cc0bcaf9ce75f9e7c682c03fc8e060861b3833
C.y <yangruihan@vip.qq.com>
2015-06-06 00:59:08 +0800

    15_6_6

---
+++ java/JavaTestPro/src/com/yrh/java/net/URLTest.java
@@ -0,0 +1,12 @@
+package com.yrh.java.net;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLTest {
+
+	public static void main(String[] args) throws MalformedURLException {
+		URL url = new URL("www.baidu.com");
+	}
+
+}
