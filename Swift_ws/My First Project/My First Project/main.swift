6bcc659dac37d26964fb1aac066e0946b1777051
Cy <yangruihan@vip.qq.com>
2015-09-21 22:59:15 +0800

    Initial Commit

---
+++ Swift_ws/My First Project/My First Project/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  My First Project
+//
+//  Created by 杨睿涵 on 15/9/21.
+//  Copyright (c) 2015年 杨睿涵. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
