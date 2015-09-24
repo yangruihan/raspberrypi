c86fc28011f1c8963b22f0362a68b05c6fcfb563
Cy <yangruihan@vip.qq.com>
2015-09-24 17:30:38 +0800

    15_9_24

---
+++ Swift_ws/Standford university/Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by 杨睿涵 on 15/9/24.
+//  Copyright © 2015年 杨睿涵. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
