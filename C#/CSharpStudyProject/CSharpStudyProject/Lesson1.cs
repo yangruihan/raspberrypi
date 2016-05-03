74c2b58a29f16fe9ddb1a86da7036db79c8f0963
C.y <yangruihan@vip.qq.com>
2016-05-03 19:59:50 +0800

    16_5_3

---
+++ C#/CSharpStudyProject/CSharpStudyProject/Lesson1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudyProject
+{
+    // 基本数据类型学习
+    class Lesson1
+    {
+        static void Study()
+        {
+            int i = 0;
+            bool b = true;
+
+            Console.WriteLine(i);
+            Console.WriteLine(b);
+
+            Console.Read();
+        }
+
+        static void Main(string[] args)
+        {
+            Study();
+        }
+    }
+}
